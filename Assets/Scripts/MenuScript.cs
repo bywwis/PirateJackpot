77fa10fab1204ed8ada460d8297e5d8d5e366992
bywwis <alinakolotova4@gmail.com>
2025-10-22 15:25:08 +0300

    открытие меню уровней из главного меню

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public Button continueButton;
+    public AudioSource clickSound;
+    public GameObject guidePage;
+
+    void Start()
+    {
+        // Проверяем, есть ли сохраненный прогресс
+        //continueButton.interactable = SaveLoadManager.LoadProgress() != null;
+    }
+
+    // Метод для кнопки "Продолжить"
+    public void ContinueGame()
+    {
+        //clickSound.Play();
+        //Invoke(nameof(LoadContinue), clickSound.clip.length);
+    }
+
+    // Метод для кнопки "Начать игру"
+    public void StartGame()
+    {
+        //clickSound.Play();
+        //Invoke(nameof(LoadNew), clickSound.clip.length);
+    }
+
+    //private void LoadContinue()
+    //{
+    //    GameProgress progress = SaveLoadManager.LoadProgress();
+    //    if (progress != null)
+    //    {
+            // Загружаем последний пройденный уровень
+    //        string lastCompletedLevel = progress.completedLevels[progress.completedLevels.Count - 1];
+    //        int nextLevelIndex = SceneUtility.GetBuildIndexByScenePath(lastCompletedLevel) + 1;
+
+    //        if (nextLevelIndex < SceneManager.sceneCountInBuildSettings - 1)
+    //        {
+    //            SceneManager.LoadScene(nextLevelIndex);
+    //        }
+    //        else
+    //        {
+    //            SceneManager.LoadScene(lastCompletedLevel);
+    //        }
+    //    }
+    //}
+
+    public void LoadNew()
+    {
+        //SaveLoadManager.ResetProgress(); // Сбрасываем прогресс
+        SceneManager.LoadScene("levelMenu"); // Загружаем первый уровень
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Игра закрыта");
+    }
+
+}
+
